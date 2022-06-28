04b7dd76206a7fbaeb856b1087105ff94c99cf6f
Viviana R <varelycode@users.noreply.github.com>
2022-06-28 15:12:17 -0400

    testing custom ui using Core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, OnInit, Output, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
 import { Subject } from 'rxjs';
-import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
+import { Core, WebViewerInstance } from '@pdftron/webviewer';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,10 @@ import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild('viewer') viewer: ElementRef;
+
+  @ViewChild('viewer') viewer: Element;
   wvInstance: WebViewerInstance;
-  @Output() coreControlsEvent:EventEmitter<string> = new EventEmitter(); 
+  @Output() coreControlsEvent: EventEmitter<string> = new EventEmitter();
 
   private documentLoaded$: Subject<void>;
 
@@ -20,37 +21,30 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
 
-    WebViewer({
-      path: '../lib',
-      initialDoc: '../files/webviewer-demo-annotated.pdf'
-    }, this.viewer.nativeElement).then(instance => {
-      this.wvInstance = instance;
+    Core.setWorkerPath('../lib/core');
+    let documentViewer: Core.DocumentViewer;
+    documentViewer = new Core.DocumentViewer();
+    documentViewer.setScrollViewElement(document.getElementById('scrollView'));
+    documentViewer.setViewerElement(document.getElementById('viewer'));
+    documentViewer.setOptions({ enableAnnotations: false });
+    documentViewer.loadDocument('/files/webview-demo-annotated.pdf');
+    documentViewer.addEventListener('documentLoaded', () => {
+      this.documentLoaded$.next();
+    });
 
-      this.coreControlsEvent.emit(instance.UI.LayoutMode.Single);
+    // WebViewer({
+    //   path: '../lib',
+    //   initialDoc: '../files/webviewer-demo-annotated.pdf'
+    // }, this.viewer.nativeElement).then(instance => {
+    //   this.wvInstance = instance;
+    //
+    //
+    //   // this.coreControlsEvent.emit(instance.UI.LayoutMode.Single);
+    //   const { documentViewer } = instance.Core;
+    //
+    //   // instance.UI.openElements(['notesPanel']);
 
-      const { documentViewer, Annotations, annotationManager } = instance.Core;
-
-      instance.UI.openElements(['notesPanel']);
-
-      documentViewer.addEventListener('annotationsLoaded', () => {
-        console.log('annotations loaded');
-      });
-
-      documentViewer.addEventListener('documentLoaded', () => {
-        this.documentLoaded$.next();
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-        annotationManager.addAnnotation(rectangleAnnot);
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
-    })
+    // });
   }
 
   ngOnInit() {
